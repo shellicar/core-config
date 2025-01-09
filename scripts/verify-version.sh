5b31a5b8692a8d277e063cd2b604c36281334ebc
Stephen Hellicar <shellicar@gmail.com>
2025-01-09 18:11:10 +1100

    Update tooling (#4)
    * Update tooling
    
    * Fix package fields

--- scripts/verify-version.sh
+++ scripts/verify-version.sh
@@ -1,14 +1,23 @@
 #!/bin/sh
+PACKAGE_NAME=$(cat .packagename)
+VERSION=$(git show :packages/$PACKAGE_NAME/package.json | node -p "JSON.parse(require('fs').readFileSync(0)).version")
+CHANGELOG=$(git show :CHANGELOG.md)
 
-VERSION=$(git show :packages/core-config/package.json | node -p "JSON.parse(require('fs').readFileSync(0)).version")
-git show :CHANGELOG.md | egrep -q "^## \[$VERSION\] - [0-9]{4}-[0-9]{2}-[0-9]{2}\$"
+# Check current version has header
+echo "$CHANGELOG" | egrep -q "^## \[$VERSION\] - [0-9]{4}-[0-9]{2}-[0-9]{2}\$"
 if [ $? -ne 0 ]; then
-  echo "Error: /^## [$VERSION] - [0-9]{4}-[0-9]{2}-[0-9]{2}\$/ not found in CHANGELOG.md"
+  echo "Error: Version header [$VERSION] not found in CHANGELOG.md"
   exit 1
 fi
 
-git show :CHANGELOG.md | egrep -q "^\[$VERSION\]: https://github.com/shellicar/.*${VERSION}\$"
-if [ $? -ne 0 ]; then
-  echo "Error: /^[$VERSION]: https://github.com/shellicar/.*${VERSION}\$/ not found in CHANGELOG.md"
-  exit 1
-fi
+# Check all version headers have link definitions
+VERSIONS=$(echo "$CHANGELOG" | grep -o "^## \[[0-9]\+\.[0-9]\+\.[0-9]\+\]" | sed 's/## \[\(.*\)\]/\1/')
+for v in $VERSIONS; do
+  echo "$CHANGELOG" | egrep -q "^\[$v\]: https://github.com/shellicar/$PACKAGE_NAME/releases/tag/$v\$"
+  if [ $? -ne 0 ]; then
+    echo "Error: Missing or invalid link definition for version [$v]"
+    exit 1
+  fi
+done
+
+exit 0
